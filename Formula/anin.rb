9871754ce8dac1822e71efd37fbfa2c1031ac7c9
MacbookPro15_Headless <255150139+buacanning@users.noreply.github.com>
2026-03-08 08:56:04 +0800

    fix: update formula hash
    - sebelumnya v pada V0.1.0 uppercase itu yang membuat hashnya jadi
    berbeda

--- Formula/anin.rb
+++ Formula/anin.rb
@@ -2,7 +2,7 @@ class Anin < Formula
   desc "Simple Vim for the Terminal on Steroids"
   homepage "https://github.com/buacanning/anin"
   url "https://github.com/buacanning/anin/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"  # Update this after uploading release
+  sha256 "fb8d17e7b7ef9c76223b342ae875d7b36b0d6f10cc019d8be8de8d0cc1c0f1bb"  # Update this after uploading release
   license "MIT"
 
   depends_on "rust" => :build
